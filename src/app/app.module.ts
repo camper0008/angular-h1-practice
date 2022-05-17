b4e82d64f22dec9e8e838a9244cb819b12feb21b
piet <tphollebeek@gmail.com>
2022-05-17 10:38:37 +0200

    form frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser"
 import { NgModule } from "@angular/core"
 
 import { AppComponent } from "./app.component"
+import { AddressFormComponent } from "./address-form/address-form.component"
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, AddressFormComponent],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
